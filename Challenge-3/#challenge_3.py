2c071832bd7eee94668e7b30030267182a8655f7
nikkss02 <nikkss_02@yahoo.co.in>
2024-05-26 14:20:07 +0100

    Create #challenge_3.py

---
+++ Challenge-3/#challenge_3.py
@@ -0,0 +1,15 @@
+#challenge_3
+def challenge_3 (arg1, arg2, seperator='/'):
+    keys= arg2.split(seperator)
+    value= arg1
+    for key in keys:
+        value= value[key]
+    return value
+
+object1= {"a":{"b":{"c":"d"}}}
+key1= "a/b/c"
+print(challenge_3(object1,key1))
+
+object2= {"x":{"y":{"z":"d"}}}
+key2= "x/y/z"
+print(challenge_3(object2,key2))
